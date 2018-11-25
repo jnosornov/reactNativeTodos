9e05ae2929f22d3e1651c50b4cf22ffda02c056f
Johnathan <jnosornov@gmail.com>
2018-11-25 14:07:55 -0500

    remove closing FlatList tag, add list item separator, and styles

--- App.js
+++ App.js
@@ -54,11 +54,18 @@ class App extends Component {
                 />
                 <View style={ styles.content }>
                     <FlatList
+                        style={ styles.list }
                         keyExtractor={ item => item.key.toString() }
                         data={ this.state.items }   
-                        renderItem={ ({ item }) => <Text>{ item.text }</Text> }
-                    >
-                    </FlatList>
+                        renderItem={ ({ item }) => {
+                            return (
+                                <View style={ styles.textContainer }>
+                                    <Text style={ styles.text }>{ item.text }</Text> 
+                                </View>
+                            );
+                        }}
+                        ItemSeparatorComponent={ () => <View style={ styles.separator } /> }
+                    />
                 </View>
                 <Footer />
             </View>
@@ -73,6 +80,23 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1
+    },
+    list: {
+        backgroundColor: "#fff"
+    },
+    separator: {
+        height: 2,
+        backgroundColor: "#f5f5f5"
+    },
+    textContainer: {
+        padding: 10,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        justifyContent: "space-between"
+    },
+    text: {
+        fontSize: 16,
+        color: "#4d4d4d"
     }
 });
 
